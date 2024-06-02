de621b3ad15d2abbe19746add76a949683ab852a
MarieAmbrazhei <marykapuza@gmail.com>
2024-06-02 14:34:15 +0300

    fixed mypy datatype check

--- homework9.py
+++ homework9.py
@@ -15,31 +15,32 @@ def validate_card(card_number: int) -> bool:
     """
     if isinstance(card_number, int):
         card_number = str(card_number)
+        even = []
+        odd = []
+        card_number_reverted = card_number[::-1]
+        for elem in range(len(card_number)):
+            if (elem + 1) % 2 == 0:
+                odd_number = int(card_number_reverted[elem]) * 2
+                if odd_number > 9:
+                    odd_number -= 9
+                odd.append(odd_number)
+
+            else:
+                even.append(int(card_number_reverted[elem]))
+
+        final_sum = sum(odd + even)
+        if final_sum % 10 == 0:
+            return True
+        return False
     else:
         print(f'Card number should have datatype int '
               f'current type is {type(card_number)}')
-    even = []
-    odd = []
-    card_number_reverted = card_number[::-1]
-    for elem in range(len(card_number)):
-        if (elem + 1) % 2 == 0:
-            odd_number = int(card_number_reverted[elem]) * 2
-            if odd_number > 9:
-                odd_number -= 9
-            odd.append(odd_number)
-
-        else:
-            even.append(int(card_number_reverted[elem]))
-
-    final_sum = sum(odd + even)
-    if final_sum % 10 == 0:
-        return True
-    return False
+        return False
 
 
 # assert validate_card(4561261212345464), 'Please provide correct card number'
 # assert validate_card(4561261212345467)
-
+# assert validate_card('4561261212345467')
 
 
 # Последовательность
