833be547f1e307ac04dfce978b5628b1d4d538e8
MarieAmbrazhei <marykapuza@gmail.com>
2024-07-19 17:53:53 +0300

    adds module conftest.py to  add custom command-line option for setting HTML log level; to configure logging based on command-line option.  Adds module constants.py; adds report.html

---
+++ homework21/conftest.py
@@ -0,0 +1,20 @@
+from pymar_logging import logger as log
+from constants import LOG_LEVELS
+
+def pytest_addoption(parser):
+    """Describes that the function adds a command-line option to set the
+     HTML log level."""
+    parser.addoption(
+        "--html-log-level",
+        action="store",
+        default="INFO",
+        help="Set the logging level for html report"
+    )
+
+
+def pytest_configure(config):
+    """Describes that the function configures logging based on the
+    command-line option"""
+    log_level = config.getoption("--html-log-level").upper()
+    if log_level_numeric := LOG_LEVELS.get(log_level):
+        log.setLevel(log_level_numeric)
