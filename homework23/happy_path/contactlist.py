dfbc6bebbb03d80f83a16a38db1d684d9e88d790
MarieAmbrazhei <marykapuza@gmail.com>
2024-08-01 08:30:19 +0300

    fix linters: change locators E1101

--- homework23/happy_path/contactlist.py
+++ homework23/happy_path/contactlist.py
@@ -20,7 +20,7 @@ print(f'Fill in the last name field: {AddUserPage.ADD_LASTNAME_INPUT}')
 print(f'Fill in the email field: {AddUserPage.LOGIN_EMAIL_INPUT}')
 print(f'Fill in the password field: {AddUserPage.LOGIN_PASSWORD_INPUT}')
 
-print(f'Click Submit button: {CommonPageLocators.LOGIN_SUBMIT_BUTTON}')
+print(f'Click Submit button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON}')
 
 # CONTACT LIST PAGE
 print(f'Click Add a New Contact button: {ContactListPage.ADD_NEW_CONTACT_BUTTON}')
@@ -37,7 +37,7 @@ print(f'Fill in the state of province field: {AddContactPage.ADD_STATE_PROVINCE_
 print(f'Fill in the postal code field: {AddContactPage.ADD_POSTAL_CODE_INPUT}')
 print(f'Fill in the country field: {AddContactPage.ADD_COUNTRY_INPUT}')
 
-print(f'Click Submit button: {CommonPageLocators.LOGIN_SUBMIT_BUTTON}')
+print(f'Click Submit button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON}')
 
 # Add another contact
 # CONTACT LIST PAGE
@@ -55,7 +55,7 @@ print(f'Fill in the state of province field: {AddContactPage.ADD_STATE_PROVINCE_
 print(f'Fill in the postal code field: {AddContactPage.ADD_POSTAL_CODE_INPUT}')
 print(f'Fill in the country field: {AddContactPage.ADD_COUNTRY_INPUT}')
 
-print(f'Click Submit button: {CommonPageLocators.LOGIN_SUBMIT_BUTTON}')
+print(f'Click Submit button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON')
 
 # Edit contact
 # CONTACT LIST PAGE
@@ -75,7 +75,7 @@ print(f'Fill in the state of province field: {ContactEdit.ADD_STATE_PROVINCE_INP
 print(f'Fill in the postal code field: {ContactEdit.ADD_POSTAL_CODE_INPUT}')
 print(f'Fill in the country field: {ContactEdit.ADD_COUNTRY_INPUT}')
 
-print(f'Click Submit button: {CommonPageLocators.LOGIN_SUBMIT_BUTTON}')
+print(f'Click Submit button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON}')
 
 # CONTACT DETAILS PAGE
 print(f'Click Return to Contact List: {ContactDetails.RETURN_TO_CONTACT_LIST_BUTTON}')
@@ -88,11 +88,11 @@ print(f'Click Delete Contact button: {ContactDetails.DELETE_CONTACT_BUTTON}')
 
 # Logout
 # CONTACT LIST PAGE
-print(f'Click Logout button: {CommonPageLocators.LOGIN_LOGOUT_BUTTON}')
+print(f'Click Logout button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON}')
 
 # Log in (if you have a user)
 # LOGIN PAGE
 print(f'Fill in the email field: {LoginPage.LOGIN_EMAIL_INPUT}')
 print(f'Fill in the password field: {LoginPage.LOGIN_PASSWORD_INPUT}')
 
-print(f'Click Submit button: {CommonPageLocators.LOGIN_SUBMIT_BUTTON}')
+print(f'Click Submit button: {CommonPageLocators.LOCATOR_SUBMIT_BUTTON}')
